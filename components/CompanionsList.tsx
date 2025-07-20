3caed37e7b325245e3b5216dda22ffc981ba2e5e
Bonnke Omorehiomwan <BonnkeWebs@gmail.com>
2025-07-20 20:03:17 +0100

    fix typescript

--- components/CompanionsList.tsx
+++ components/CompanionsList.tsx
@@ -1,13 +1,13 @@
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
 import { cn, getSubjectColor } from "@/lib/utils";
+import { Companion } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -24,7 +24,7 @@ const CompanionsList = ({
 }: CompanionsListProps) => {
   return (
     <article className={cn("companion-list", classNames)}>
-      <h2 className="font-bold text-3xl">Recent Sessions</h2>
+      <h2 className="font-bold text-3xl">{title}</h2>
       <Table>
         <TableHeader>
           <TableRow>
