9824f92d5c712bcd41afa6c5215557cc6dca4ef6
Bonnke Omorehiomwan <BonnkeWebs@gmail.com>
2025-08-14 16:02:41 +0100

    implement sidebar

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-const navItems = [
+export const navItems = [
   { label: "Home", href: "/" },
   { label: "Companions", href: "/companions" },
   { label: "My Journey", href: "/my-journey" },
@@ -12,7 +12,7 @@ const navItems = [
 const NavItems = () => {
   const pathname = usePathname();
   return (
-    <nav className="flex items-center gap-4 whitespace-nowrap">
+    <nav className="hidden lg:flex items-center gap-4 whitespace-nowrap">
       {navItems.map((item) => {
         return (
           <Link
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,20 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
-import NavItems from "./NavItems";
+import NavItems, { navItems } from "./NavItems";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 const Navbar = () => {
+  const pathname = usePathname();
   return (
     <nav className="navbar">
       <Link href="/">
@@ -11,7 +22,7 @@ const Navbar = () => {
           <Image src="/images/logo.svg" alt="logo" width={46} height={44} />
         </div>
       </Link>
-      <div className="flex items-center gap-8">
+      <div className="flex items-center gap-4">
         <NavItems />
         <SignedOut>
           <div className="flex items-center gap-2 cursor-pointer">
@@ -21,6 +32,40 @@ const Navbar = () => {
         <SignedIn>
           <UserButton afterSwitchSessionUrl="/" />
         </SignedIn>
+        <Sheet>
+          <SheetTrigger className="lg:hidden">
+            <Menu className="w-8 h-6" />
+          </SheetTrigger>
+          <SheetContent className="flex flex-col items-start gap-18 p-6 lg:hidden">
+            <SheetTitle>
+              <Link href="/">
+                <div className="flex items-center gap-2.5 cursor-pointer">
+                  <Image
+                    src="/images/logo.svg"
+                    alt="logo"
+                    width={46}
+                    height={44}
+                  />
+                </div>
+              </Link>
+            </SheetTitle>
+            <nav className="lg:hidden flex flex-col gap-4 whitespace-nowrap">
+              {navItems.map((item) => {
+                return (
+                  <Link
+                    href={item.href}
+                    key={item.label}
+                    className={cn(
+                      pathname === item.href && "text-primary font-semibold"
+                    )}
+                  >
+                    {item.label}
+                  </Link>
+                );
+              })}
+            </nav>
+          </SheetContent>
+        </Sheet>
       </div>
     </nav>
   );
